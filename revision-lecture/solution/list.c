bc9a51fc79a08b6d9cea3e52f2c33722d7b6524b
Adam T <adam.tizzone@gmail.com>
2018-06-06 15:57:05 +1000

    Added part of the revision lecture solution

--- revision-lecture/solution/list.c
+++ revision-lecture/solution/list.c
@@ -41,10 +41,9 @@ void print_list_recursively(struct node *list_head) {
   if (list_head != NULL) {
     printf("%d", list_head->data);
     if (list_head->next != NULL) {
-      printf(",");
+      printf(" ");
     }
-    printf(" ");
-    print_list_recursively(list_head->next);
+    print_list(list_head->next);
   } else {
     printf("(end)");
   }
@@ -80,7 +79,6 @@ int is_positive_or_negative_integer(int value) {
 }
 
 struct node *get_all_even_values(struct node *list) {
-  // There is a subtle bug here. Can you spot it? Use GDB to find it!
   if (list == NULL) {
     return NULL;
   }
@@ -116,34 +114,20 @@ struct node *get_all_odd_values(struct node *list) {
   }
   struct node *curr = list;
   struct node *odd_list_head = NULL;
-  // find the first odd element
   while (curr != NULL) {
     if (is_odd(curr->data) == TRUE && is_positive_or_negative_integer(curr->data) == TRUE) {
       odd_list_head = create_node(curr->data);
-      // exit out of the loop when the first item in the odd list is
-      // found
       break;
     }
     curr = curr->next;
   }
-  // assign the current odd list pointer to the head of the newly
-  // constructed list containing the odd elements of the input list
   struct node *odd_list_curr = odd_list_head;
 
   if (curr == NULL) {
-    // no items left in the list after the first item
     return odd_list_head;
   } else if (curr->next == NULL) {
-    // if the list is two items long, check if the second item is a
-    // candidate
     if (is_odd(curr->data) == TRUE && is_positive_or_negative_integer(curr->data) == TRUE) {
-      // if the second item in the list is also odd, 
-      // There is a bug here. Can you spot it? Use GDB to find it!
-      if (odd_list_head == NULL) {
-        odd_list_head = create_node(curr->data);
-      } else {
-        odd_list_head->next = create_node(curr->data);
-      }
+      odd_list_head = create_node(curr->data);
       return odd_list_head;
     }
   }
@@ -157,24 +141,4 @@ struct node *get_all_odd_values(struct node *list) {
   }
 
   return odd_list_head;
-}
-
-int count_common_elements(struct node *list_one, struct node *list_two) {
-  if (list_one == NULL || list_two == NULL) {
-    return 0;
-  }
-  // guaranteed that neither list is NULL and thus both input variables
-  // may safely be dereferenced
-  if (list_one->data == list_two->data) {
-    // move to the next item in both lists
-    return 1 + count_common_elements(list_one->next, list_two->next);
-  } else if (list_one->data < list_two->data) {
-    // move to the next item in the list with the lower current value
-    // only
-    return count_common_elements(list_one->next, list_two);
-  } else {
-    // move to the next item in the list with the lower current value
-    // only
-    return count_common_elements(list_one, list_two->next);
-  }
 }
